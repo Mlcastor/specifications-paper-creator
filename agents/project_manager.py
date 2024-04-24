2d6adb8ca3193eba86cca8be842bc78d7397e6a3
mlcastor <jeremie.escach74@gmail.com>
2024-04-24 14:27:00 +0200

    Refactor project_manager agent class in agents/project_manager.py

--- agents/project_manager.py
+++ agents/project_manager.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 
+from utils.utils import print_agent_output
+
 from crewai import Agent
 from langchain_groq import ChatGroq
 
@@ -14,6 +16,12 @@ class project_manager(Agent):
     def __init__(self):
         super().__init__(
             role="project manager agent",
-            goal="""Gather information about the project by asking questions to the client to tailor a specification document. \
-                """,
+            goal="Gather information about the project by asking questions to the client to tailor a specification document.",
+            backstory="you are the project manager of a software development team. You have been tasked with gathering information about the project from the client to tailor a specification document.",
+            llm=GROQ_LLM,
+            verbose=True,
+            allow_delegation=False,
+            max_iter=5,
+            memory=True,
+            step_callback=lambda x: print_agent_output(x, "Email Categorizer Agent"),
         )
